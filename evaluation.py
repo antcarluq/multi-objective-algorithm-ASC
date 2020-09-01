b25202947ad9eed0a1a638203143a6962cb2e1e7
antcarluq <antcarluq@alum.us.es>
2020-09-01 18:03:16 +0200

    Implementado una metodo para realizar la evaluacion automatica y la generacion de resultados

---
+++ evaluation.py
@@ -0,0 +1,58 @@
+import numpy as numpy
+import random
+import math
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+
+from main import *
+from subproblem import Subproblem
+from subproblem import Individual
+
+
+evaluations = [(40, 100), (80, 50), (100, 40), (40, 250), (100, 100), (200, 50)]
+seeds = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+#ZDT3
+##########################################
+for evaluation in evaluations:
+    for seed in seeds:
+        generations = evaluation[1]
+        population = evaluation[0]
+        iterations = generations * population
+        neigbours = int(evaluation[0] * 0.3)
+        subproblems = algorithm(generations, population, neigbours, [0, 1], 30, "zdt3", seed)
+        f = open("results/MyAlgorithm/ZDT3/EVAL" + str(iterations) + "/P" + str(population) + "G" + str(generations) + "/final_pop_seed" + str(seed) + ".out", "w")
+        for subproblem in subproblems:
+            f.write(str(subproblem.individual.solution[0]) + " " + str(subproblem.individual.solution[1]) + "\n")
+        f.close()
+##########################################
+
+#CF6 4D
+##########################################
+for evaluation in evaluations:
+    for seed in seeds:
+        generations = evaluation[1]
+        population = evaluation[0]
+        iterations = generations * population
+        neigbours = int(evaluation[0] * 0.3)
+        subproblems = algorithm(generations, population, neigbours, [-2, 2], 4, "cf6", seed)
+        f = open("results/MyAlgorithm/CF64D/EVAL" + str(iterations) + "/P" + str(population) + "G" + str(generations) + "/final_pop_seed" + str(seed) + ".out", "w")
+        for subproblem in subproblems:
+            f.write(str(subproblem.individual.solution[0]) + " " + str(subproblem.individual.solution[1]) + "\n")
+        f.close()
+##########################################
+
+#CF6 16D
+##########################################
+for evaluation in evaluations:
+    for seed in seeds:
+        generations = evaluation[1]
+        population = evaluation[0]
+        iterations = generations * population
+        neigbours = int(evaluation[0] * 0.3)
+        subproblems = algorithm(generations, population, neigbours, [-2, 2], 16, "cf6", seed)
+        f = open("results/MyAlgorithm/CF616D/EVAL" + str(iterations) + "/P" + str(population) + "G" + str(generations) + "/final_pop_seed" + str(seed) + ".out", "w")
+        for subproblem in subproblems:
+            f.write(str(subproblem.individual.solution[0]) + " " + str(subproblem.individual.solution[1]) + "\n")
+        f.close()
+##########################################
--- prueba.py
+++ prueba.py
@@ -9,28 +9,4 @@ from subproblem import Subproblem
 from subproblem import Individual
 
 
-g = 250
-n = 40
-t = 12
-type = "zdt3"
-dimension = 30
-search_space = [0, 1]
-seed = 30
-
-
-subproblems = algorithm(g, n, t, search_space, dimension, type, seed)
-
-nsgaii = numpy.genfromtxt('results/ZDT3/EVAL10000/P40G250/final_pop_seed1.out')
-pareto_front = numpy.genfromtxt('ZDT3_PF.dat')
-plt.plot(pareto_front[:, 0], pareto_front[:, 1], 'bo', markersize=4, color="black")
-plt.plot(nsgaii[:, 0], nsgaii[:, 1], 'bo', markersize=4, color="red")
-for subproblem in subproblems:
-        plt.plot(subproblem.individual.solution[0], subproblem.individual.solution[1], 'go')
-plt.axis((-0.05, 1, -1, 5))
-plt.show(block=False)
-
-
-f = open("results/MyAlgorithm/EVAL10000/P"+ str(n) +"G"+ str(g) +"/final_pop_seed"+ str(seed) +".out","w")
-for subproblem in subproblems:
-    f.write(subproblem.individual.solution[0] + " " + subproblem.individual.solution[1] + "\n")
-f.close()
+print(int(2.111))
