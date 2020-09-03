740e721fc67e0eacf78d3fd7477a3655ee911833
antcarluq <antcarluq@alum.us.es>
2020-09-03 19:59:34 +0200

    Formateado los resultados

--- metrics.py
+++ metrics.py
@@ -10,7 +10,7 @@ response_two = "0" # Quiero estudiar 1 generacion (0) o todas (1)
 response_three = "2" # Numero de objetivos
 response_six = "1" # Generacion a estudiar del primer archivo
 response_nine = "1" # Generacion a estudiar del segundo archivo
-reponse_ten = "0" # Quiero que se cree un punto de referencia para el hiper volumen
+reponse_ten = "1" # Quiero que se cree un punto de referencia automatico para el hiper volumen
 
 
 ##########################################
@@ -22,7 +22,11 @@ for problem in problems:
             iterations = population * generations
             response_four = "results/MyAlgorithm/"+ problem +"/EVAL" + str(iterations) + "/P" + str(population) + "G" + str(generations) + "/final_pop_seed" + str(seed) + ".out" # Ruta del primer archivo
             response_five = str(population) # Poblacion del primer archivo
-            response_seven = "results/NSGAII/"+ problem +"/EVAL" + str(iterations) + "/P" + str(population) + "G" + str(generations) + "/all_popm_seed" + str(seed) + ".out" # Ruta del segundo archivo
+            if problem == "ZDT3":
+                response_seven = "results/NSGAII/"+ problem +"/EVAL" + str(iterations) + "/P" + str(population) + "G" + str(generations) + "/all_popm_seed" + str(seed) + ".out" # Ruta del segundo archivo
+            else:
+                response_seven = "results/NSGAII/"+ problem +"/EVAL" + str(iterations) + "/P" + str(population) + "G" + str(generations) + "/allpopm_seed" + str(seed) + ".out" # Ruta del segundo archivo
+
             response_eight = str(population) # Poblacion del segundo archivo
             response_nine = str(generations)  # Generacion a estudiar del segundo archivo
 
@@ -44,7 +48,10 @@ for problem in problems:
             result = os.popen("metrics/metrics < ./" + ruta_input_file).read()
             r = open("results/comparation/"+ problem +"/EVAL" + str(iterations) + "/P" + str(population) + "G" + str(
                 generations) + "/seed" + str(seed) + ".txt", "w+")
-            r.write(result)
+            lines = result.split("\n")
+            r.write("1-> New algorithm\n2-> NSGAII\n\n")
+            for line in lines[10:]:
+                r.write(line + "\n")
             r.close()
 
 #######################################
